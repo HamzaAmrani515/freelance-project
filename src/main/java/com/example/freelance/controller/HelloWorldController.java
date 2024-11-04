9c968c87da193adce03357a81b12a76f06ee0ea0
assala <assalahamoudi1@gmail.com>
2024-11-04 23:34:10 +0100

    manage unit test

--- src/main/java/com/example/freelance/controller/HelloWorldController.java
+++ src/main/java/com/example/freelance/controller/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping
     public String hello() {
-        return "j'aime mon bébé yanis. \n";
+        return "hello i am Assala Hamoudi V3. \n";
     }
 }
---
+++ src/test/java/com/example/freelance/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com.example.freelance;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    void addition_test() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    void soustraction_test() {
+        Assertions.assertEquals(2, 4 - 2);
+    }
+}
