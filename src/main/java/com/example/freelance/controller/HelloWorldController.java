eeed4484604db0c0b88497d16b96ad3122c8436d
assala <assalahamoudi1@gmail.com>
2024-11-04 23:01:38 +0100

    test deploy with service

--- src/main/java/com/example/freelance/controller/HelloWorldController.java
+++ src/main/java/com/example/freelance/controller/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping
     public String hello() {
-        return "hello i am Assala Hamoudi V2. \n";
+        return "j'aime mon bébé yanis. \n";
     }
 }
