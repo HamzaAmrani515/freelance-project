09b379fcde7f9e3b26abff721b597cb55d615919
assala <assalahamoudi1@gmail.com>
2024-11-06 18:39:34 +0100

    manage logs

--- src/main/java/com/example/freelance/controller/HelloWorldController.java
+++ src/main/java/com/example/freelance/controller/HelloWorldController.java
@@ -14,6 +14,6 @@ public class HelloWorldController {
     @GetMapping
     public String hello() {
         logger.info("Hello je suis le log spring boot.");
-        return "hello i am Assala Hamoudi V7. \n";
+        return "hello i am Assala Hamoudi V10. \n";
     }
 }
