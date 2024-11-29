a34f4779e9242d404fe5fb73830f2c971af49aed
assala <assalahamoudi1@gmail.com>
2024-11-29 07:48:01 +0100

    configure pprod

--- src/main/java/com/example/freelance/controller/FreelanceController.java
+++ src/main/java/com/example/freelance/controller/FreelanceController.java
@@ -33,12 +33,12 @@ public class FreelanceController {
         freelanceService.deleteFreelance(id);
     }
 
-
-    @DeleteMapping("/{name}")
+/*
+    @DeleteMapping("/test/{name}")
     public void deleteFreelanceByName(@PathVariable String name) {
 
     }
-
+*/
 
 
     @PutMapping("/{id}")
