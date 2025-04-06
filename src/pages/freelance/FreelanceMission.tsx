23e4988d72c10c12e8aee7df4a2853a03a3d856b
assala <assalahamoudi1@gmail.com>
2025-04-07 01:23:16 +0200

    supprimer

--- src/pages/freelance/FreelanceMission.tsx
+++ src/pages/freelance/FreelanceMission.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import { RecommendationControllerService } from "../../api";
 import type { Mission } from "../../api/models/Mission";
