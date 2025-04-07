3b85392c6386ab31c36202ab7e39c4b8c7aba402
assala <assalahamoudi1@gmail.com>
2025-04-07 23:48:25 +0200

    ajuster le test

--- src/test/java/com/example/freelance/service/RecommendationServiceTest.java
+++ src/test/java/com/example/freelance/service/RecommendationServiceTest.java
@@ -43,12 +43,12 @@ class RecommendationServiceTest {
 
     @Test
     @DisplayName("faire une  IllegalArgumentException si la mission n'est pas trouver")
-    void recommendFreelancersForMission_MissionNotFound() {
-        // GIVEN
+    void recommendFreelancersForMissionExistePas() {
+
         Long missionId = 1L;
         when(missionRepository.findById(missionId)).thenReturn(java.util.Optional.empty());
 
-        // WHEN & THEN
+
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
                 recommendationService.recommendFreelancersForMission(missionId)
         );
@@ -57,7 +57,7 @@ class RecommendationServiceTest {
 
     @Test
     @DisplayName("retourner une liste vide si il a pas de lmission similaire")
-    void recommendFreelancersForMission_NoSimilarMissions() {
+    void recommendFreelancersForMissionPasdeSimilarMissions() {
         // GIVEN ( création d'un jeux de données )
         Long missionId = 1L;
         Competence competence = new Competence();
@@ -72,17 +72,17 @@ class RecommendationServiceTest {
         when(missionRepository.findSimilarMissionIds(anySet(), eq(missionId), anyInt()))
                 .thenReturn(Collections.emptyList());
 
-        // WHEN
+
         List<FreelancerRecommendationDTO> result = recommendationService.recommendFreelancersForMission(missionId);
 
-        // THEN
+
         assertTrue(result.isEmpty());
     }
 
     @Test
     @DisplayName("retourner une liste vite si il trouve pas d'evaluation ")
-    void recommendFreelancersForMission_NoEvaluations() {
-        // GIVEN
+    void recommendFreelancersForMissionpasdEvaluations() {
+
         Long missionId = 1L;
         Competence competence = new Competence();
         competence.setId(100L);
@@ -98,17 +98,17 @@ class RecommendationServiceTest {
         when(evaluationRepository.findAllByMissionIds(anyList()))
                 .thenReturn(Collections.emptyList());
 
-        // WHEN
+
         List<FreelancerRecommendationDTO> result = recommendationService.recommendFreelancersForMission(missionId);
 
-        // THEN
+
         assertTrue(result.isEmpty());
     }
 
 
     @Test
     @DisplayName("retourner une liste vide si le freelance n'a pas assez de competence ")
-    void recommendFreelancersForMission_NotEnoughCommonCompetences() {
+    void recommendFreelancersForMissionNotEnoughCommonCompetences() {
         // GIVEN
         Long missionId = 1L;
         Competence competence1 = new Competence();
@@ -142,17 +142,17 @@ class RecommendationServiceTest {
         when(freelancerRepository.findAllWithCompetencesByIdIn(Set.of(10L)))
                 .thenReturn(List.of(freelancer));
 
-        // WHEN
+
         List<FreelancerRecommendationDTO> result = recommendationService.recommendFreelancersForMission(missionId);
 
-        // THEN
+
         assertTrue(result.isEmpty());
     }
 
     @Test
     @DisplayName("retourner le freelance recommander avec le score calculer ")
 
-    void recommendFreelancersForMission_ValidRecommendation() {
+    void recommendFreelancersForMissionRecommendationtrouver() {
         Field field = ReflectionUtils.findField(RecommendationService.class, "MIN_SIMILAR");
         if (field != null) {
             ReflectionUtils.makeAccessible(field); // rend accessible le champ privé
@@ -200,10 +200,10 @@ class RecommendationServiceTest {
         when(freelancerRepository.findAllWithCompetencesByIdIn(Set.of(10L)))
                 .thenReturn(List.of(freelancer));
 
-        // WHEN
+
         List<FreelancerRecommendationDTO> result = recommendationService.recommendFreelancersForMission(missionId);
 
-        // THEN
+
         //assertion pour faire les vérification
         assertNotNull(result);
         assertEquals(1, result.size());
@@ -212,13 +212,7 @@ class RecommendationServiceTest {
         assertEquals("Doe", recommendation.getNom());
         assertEquals("John", recommendation.getPrenom());
 
-        /*
-         * Calcul du score attendu :
-         * - Nombre de compétences en commun (commonCount) = 2
-         * - Expérience = 3.0
-         * - Note moyenne = (4.0 + 4.0) / 2 = 4.0
-         * Score = (2 * 2) + 3.0 + (4.0 * 2) = 4 + 3 + 8 = 15
-         */
+
         assertEquals(15, recommendation.getScore());
     }
 }
